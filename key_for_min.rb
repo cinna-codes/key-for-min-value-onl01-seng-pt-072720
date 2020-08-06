4bf40c0e3e82fc0e0888a6768b883f30b95ebe79
Cinna Goldman <somethingfellipickeditup@gmail.com>
2020-08-06 04:19:19 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,13 +4,13 @@
 def key_for_min_value(name_hash)
   lowest_key = nil
   lowest_value = nil
-  
+
   name_hash.each do |k, v|
     if lowest_value == nil || v < lowest_value
      lowest_value = v
      lowest_key = k
     end
-  
+
   end
   key
 end
